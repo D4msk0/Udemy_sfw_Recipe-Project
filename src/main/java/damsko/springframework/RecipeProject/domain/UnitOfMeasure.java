2a1802fa6dfab2989e89b00a2047bb63ef58b837
Merry Damsko <m.d4msk0@gmail.com>
2024-05-01 13:35:38 +0200

    added uom and set relationship with ingredient

--- src/main/java/damsko/springframework/RecipeProject/domain/UnitOfMeasure.java
+++ src/main/java/damsko/springframework/RecipeProject/domain/UnitOfMeasure.java
@@ -1,25 +1,19 @@
 package damsko.springframework.RecipeProject.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 
 @Getter
 @Setter
 @Entity
-public class Ingredient {
+public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-    private BigDecimal amount;
-
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure uom;
-
-    @ManyToOne
-    private Recipe recipe;
 }
