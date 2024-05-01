ea362c9debf3f6dad9b52088ca4aa31efaf3477b
Merry Damsko <m.d4msk0@gmail.com>
2024-05-01 21:28:09 +0200

    update indexcontroller

--- src/main/java/damsko/springframework/RecipeProject/controllers/IndexController.java
+++ src/main/java/damsko/springframework/RecipeProject/controllers/IndexController.java
@@ -1,14 +1,33 @@
 package damsko.springframework.RecipeProject.controllers;
 
+import damsko.springframework.RecipeProject.domain.Category;
+import damsko.springframework.RecipeProject.domain.UnitOfMeasure;
+import damsko.springframework.RecipeProject.repositories.CategoryRepository;
+import damsko.springframework.RecipeProject.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String getIndexPage(){
-        System.out.println("some message to say...");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/damsko/springframework/RecipeProject/repositories/CategoryRepository.java
+++ src/main/java/damsko/springframework/RecipeProject/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package damsko.springframework.RecipeProject.repositories;
 import damsko.springframework.RecipeProject.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
