8bb0525514152319174e60cc027e13c49bd99247
Merry Damsko <m.d4msk0@gmail.com>
2024-05-01 12:11:39 +0200

    Created two entities Notes and Recipe set Id set OneToOne relationships set @Lob for large String values

---
+++ src/main/java/damsko/springframework/RecipeProject/domain/Notes.java
@@ -0,0 +1,17 @@
+package damsko.springframework.RecipeProject.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+}
---
+++ src/main/java/damsko/springframework/RecipeProject/domain/Recipe.java
@@ -0,0 +1,31 @@
+package damsko.springframework.RecipeProject.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+}
