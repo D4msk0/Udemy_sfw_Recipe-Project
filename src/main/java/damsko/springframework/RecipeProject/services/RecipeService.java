a238a843fbbef8af8b89de1f012e683a4bdaf4cc
Merry Damsko <m.d4msk0@gmail.com>
2024-05-06 22:19:30 +0200

    update a recipe

--- src/main/java/damsko/springframework/RecipeProject/services/RecipeService.java
+++ src/main/java/damsko/springframework/RecipeProject/services/RecipeService.java
@@ -2,7 +2,6 @@ package damsko.springframework.RecipeProject.services;
 
 import damsko.springframework.RecipeProject.commands.RecipeCommand;
 import damsko.springframework.RecipeProject.domain.Recipe;
-import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
@@ -12,6 +11,8 @@ public interface RecipeService {
     Set<Recipe> getRecipes();
 
     Recipe findById(long id);
+
+    RecipeCommand findCommandById(Long l);
 
     RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
